05039ac5c0172f3f111a8277e1b0f816ecdaf6b2
iamiz <iqramanroo54@gmail.com>
2022-04-24 09:46:40 +0530

    updated rendering of prediction dashboard

--- dash_predict.py
+++ dash_predict.py
@@ -16,18 +16,27 @@ from dash_utils import *
 
 np.set_printoptions(precision=10)
 
+printmd('<h2>Welcome to fundamental period prediction dashboard!</h2>')
+printmd('<h3>Follow these steps to predict the fundamental period of masonary infilled RC frame.</h3>')
+printmd('<b>Step 1:</b> Enter the values of all the input features.')
+printmd("<b>Step 2:</b> Click to select the prediction model.")
+printmd("<b>Step 3:</b> Click on Get Prediction!.")
+
 @interact
-def predict(NOSt = widgets.BoundedIntText(value = 22, min = 1, max = 22.0, step = 1, description = 'NOSt', disabled = False),
-                     OR = widgets.BoundedFloatText(value = 1., min = 0., max = 1.0, step = 0.01, description = 'OR', disabled = False),
-                     SL = widgets.BoundedFloatText(value = 7.5, min = 3, max = 10.0, step = 0.01, description = 'SL', disabled = False),
-                     K = widgets.BoundedFloatText(value = 15., min = 2.25, max = 25.0, step = 0.01, description = 'K', disabled = False),
-                     NOSp= widgets.BoundedIntText(value = 2, min = 1, max = 6.0, step = 1, description = 'NOSp', disabled = False),
+def predict(NOSt = widgets.BoundedIntText(value = 22, min = 1, max = 22.0, step = 1, description = '<b>NOSt</b>', disabled = False),
+                     OR = widgets.BoundedFloatText(value = 1., min = 0., max = 1.0, step = 0.01, description = '<b>OR</b>', disabled = False),
+                     SL = widgets.BoundedFloatText(value = 7.5, min = 3, max = 10.0, step = 0.01, description = '<b>SL</b>', disabled = False),
+                     K = widgets.BoundedFloatText(value = 15., min = 2.25, max = 25.0, step = 0.01, description = '<b>K</b>', disabled = False),
+                     NOSp= widgets.BoundedIntText(value = 2, min = 1, max = 6.0, step = 1, description = '<b>NOSp</b>', disabled = False),
                     Model = widgets.ToggleButtons(
     options=['NN', 'XGBoost', 'Eq. (9)'],
-    description = ' Model',
+    description = '<b>Model<b>',
     disabled = False,
     button_style='info', # 'success', 'info', 'warning', 'danger' or ''
-    tooltips=['Neural Network', 'XGBoost', 'Equation given by Charalampakis et al. [17]'])):
+    tooltips=['Neural Network', 'XGBoost', 'Equation given by Charalampakis et al. [17]'],
+                    style=dict(font_weight='bold'))):
+    
+    
     
     columns=['NOSt', 'NOSp', 'SL', 'OR', 'K']
     
@@ -60,7 +69,7 @@ def predict(NOSt = widgets.BoundedIntText(value = 22, min = 1, max = 22.0, step 
                 WS = df.K
                 tpred = (0.067+0.16*OR - 0.159*OR**2 + 0.002*SL**1.71)*(N**(0.95-0.004*OR+0.12*OR**2)/WS**(0.39-0.50*OR+0.110*OR**2))
                 print(f'\n \n Predicted fundamental period using {Model}: ',round(tpred,4).values.item(), 's')
-    button = widgets.Button(description="Get prediction",button_style = 'success',tooltip = 'Description', icon = 'compact-disc')
+    button = widgets.Button(description="Get prediction",button_style = 'success',tooltip = 'Description', icon = 'compact-disc',style=dict(font_weight='bold'))
     
     output = widgets.Output()
 
